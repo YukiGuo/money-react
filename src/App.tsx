dd1da70946a59d81ed97c3dbc0148bc747fc40ff
Guo <2540865175@qq.com>
2020-05-26 22:27:08 +0800

    拆分成四个View组件

--- src/App.tsx
+++ src/App.tsx
@@ -6,64 +6,35 @@ import {
     Route,
     Redirect
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import UnFound from './view/Unfound';
+import Money from './view/Money';
+import Tags from './view/Tags';
+import Bill from './view/Bill';
+import Statistics from './view/Statistics';
 
 function App() {
     return (
         <Router>
-                        <Switch>
-                            <Route path="/money">
-                                <Money/>
-                            </Route>
-                            <Route path="/tags">
-                                <Tags/>
-                            </Route>
-                            <Route path="/statistics">
-                                <Statistics/>
-                            </Route>
-                            <Route path="/bill">
-                                <Bill/>
-                            </Route>
-                            <Redirect exact from ='/' to='/money' />
-                            <Route path="*">
-                                <UnFound/>
-                             </Route>
-                        </Switch>
+            <Switch>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Route path="/bill">
+                    <Bill/>
+                </Route>
+                <Redirect exact from='/' to='/money'/>
+                <Route path="*">
+                    <UnFound/>
+                </Route>
+            </Switch>
         </Router>
     );
 }
 
-function Statistics() {
-    return (
-        <Layout>
-            <h2>Statistics</h2>
-        </Layout>
-    );
-}
-
-function Money() {
-
-    return (
-        <Layout>
-            <h2>Money</h2>
-        </Layout>
-    )
-}
-function Tags() {
-    return (
-        <Layout>
-            <h2>Tags</h2>
-        </Layout>
-    )
-}
-function UnFound() {
-    return <h4>该页面不存在</h4>;
-}
-function Bill() {
-    return (
-        <Layout>
-            <h2>Bill</h2>
-        </Layout>
-    )
-}
 export default App;
---
+++ src/view/Unfound.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function UnFound() {
+    return <h4>该页面不存在</h4>;
+}
+export default UnFound;
