5174a2f044b63f2a1f29b0eac4fbb69b67793c3f
Guo <2540865175@qq.com>
2020-05-28 22:56:48 +0800

    note组件基本功能实现

--- src/components/money/Note.tsx
+++ src/components/money/Note.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-
-function Note() {
+import React,{useState,useRef} from 'react';
+import styled from 'styled-components';
+const Wrapper=styled.div`
+min-height:52px;
+background:#ddd;
+display:flex;
+ align-items: center;
+ >input{
+ flex-grow:1;
+ border:none;
+ height:40px;
+ background:#ddd;
+padding:6px;
+&:focus{
+outline:none
+}
+ }
+`;
+const Note:React.FC =() => {
+const [note,setNote]=useState<string>('');
+const refInput =useRef<HTMLInputElement>(null)
+const change =()=>{
+if( refInput.current!==null ){
+    setNote(refInput.current.value)
+}
+}
 return(
-    <div> </div>
+    <Wrapper>
+         <span>备注</span>
+         <input
+         type='text' placeholder="请填写备注"
+         defaultValue={note}
+         onBlur={change}
+         ref={refInput}
+         ></input>
+     </Wrapper>
 )
 }
 export default Note;
