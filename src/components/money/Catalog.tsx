8d1eaa01946a3c8ee89242d2d45dad9b8a367eab
Guo <2540865175@qq.com>
2020-05-28 23:37:06 +0800

    Catalog组件基本功能实现

---
+++ src/components/money/Catalog.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Wrapper=styled.div`
+width: 100%;
+height: 56px;
+display: flex;
+align-items: center;
+>span{
+display: inline-flex;
+height: 100%;
+width: 50%;
+justify-content: center;
+align-items: center;
+position: relative;
+&.selected{
+background: #eee;
+
+&::after{
+content:'';
+position: absolute;
+bottom: 0;
+left: 0;
+width: 100%;
+height: 2px;
+background: #333;
+}
+}
+}
+`;
+const Catalog:React.FC=()=> {
+    const catalogHash={'+':'支出', '-':'收入'};
+    const[catalog,setCatalog]=useState<string>('-');
+    const [catalogList]=useState<('+'|'-')[]>(['+','-']);
+    return(
+        <Wrapper>
+            {catalogList.map(t=>
+            <span
+                className={t===catalog?'selected':''}
+                onClick={()=>setCatalog(t)}
+            >
+                {catalogHash[t]}</span>
+            )}
+
+        </Wrapper>
+    )
+};
+export default Catalog;
--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import NumberPad from '../components/money/NumberPad';
 import Tag from '../components/money/Tag';
 import Note from '../components/money/Note';
+import Catalog from '../components/money/Catalog';
 function Money() {
 
     return (
         <Layout>
             <Tag/>
             <Note/>
+            <Catalog/>
              <NumberPad/>
         </Layout>
     )
